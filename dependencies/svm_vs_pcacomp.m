312b2b709090087c902b40b128664d82476fb200
tyo8 <47438759+tyo8@users.noreply.github.com>
2021-05-09 17:13:46 -0500

    Add files via upload

--- dependencies/svm_vs_pcacomp.m
+++ dependencies/svm_vs_pcacomp.m
@@ -1,7 +1,7 @@
 % Note: assumes X is in PCA basis and columns are sorted by decreasing
 % variance explained.
 
-function [svm_acc] = svm_vs_pcacomp(X,Y)
+function [svm_loss,n_dims] = svm_vs_pcacomp(X,Y)
 
 n = round(size(X,2)/5);
 
@@ -9,13 +9,13 @@ subsamp = linspace(0,log10(size(X,2)),n);
 
 n_dims = unique(round(10.^subsamp));
 
-svm_acc = zeros(size(n_dims));
+svm_loss = zeros(size(n_dims));
 
 for i=1:length(n_dims)
-    Mdl = fitcsvm(X(:,1:n_dims(i)),Y,'KernelFunction','linear',...
+    Mdl = fitcsvm(X(:,1:n_dims(i)),Y,'KernelFunction','gaussian',...
         'BoxConstraint',1e-3,'KFold',5,'Standardize',false);
     
-    svm_acc(i)=kfoldLoss(Mdl);
+    svm_loss(i)=kfoldLoss(Mdl);
 end
 
 end
